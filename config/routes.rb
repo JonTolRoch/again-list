3a633ce79cfb44200d3136e8e0c91ee56f7b9e2b
Joao Toledo <j.toledo.rocha96@gmail.com>
2023-07-13 19:41:19 -0300

    Already working

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,14 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "lists#index"
+  get "/lists", to: "lists#index", as: "lists"
+  get "/lists/new",to: "lists#new", as: "new_list"
+  get "/lists/:id", to: "lists#show", as: "list"
+  post "lists",to:"lists#create"
+  delete "lists/:id",to:"lists#destroy",as: "delete_list"
+  get "/lists/:list_id/bookmarks/new",to:"bookmarks#new",as:"new_bookmark"
+  post "/lists/:list_id/bookmarks",to:"bookmarks#create",as:"create_bookmark"
+  delete "bookmarks/:id",to:"bookmarks#destroy",as:"delete_bookmark"
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+require "json"
+
+puts "Cleaning up database..."
+Movie.destroy_all
+puts "Database cleaned"
+
+def generate_movies(count)
+  base_url = "https://image.tmdb.org/t/p/"
+  poster_size = "w185"
+
+  url = "http://tmdb.lewagon.com/movie/popular"
+  count.times do |i|
+    puts "Importing Movies from page #{i + 1}"
+    movies = JSON.parse(URI.open("#{url}?page=#{i + 1}").read)['results']
+    movies.each do |movie|
+      puts "Creating #{movie['title']}"
+      poster_path = movie['poster_path']
+      poster_url = "#{base_url}#{poster_size}/#{poster_path}"
+      Movie.create(
+        title: movie['title'],
+        overview: movie['overview'],
+        poster_url: poster_url,
+        rating: movie['vote_average']
+      )
+    end
+  end
+  puts "Movies Created"
+end
+
+generate_movies(10)
